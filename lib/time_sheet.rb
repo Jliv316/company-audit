feed122ccef74472109be7e5c5a5d522cdc66c9e
John Livingston <jclivingston316@gmail.com>
2018-04-17 11:27:42 -0600

    iteratoin_1 complete

--- lib/time_sheet.rb
+++ lib/time_sheet.rb
@@ -1,9 +1,18 @@
 require 'date'
 class TimeSheet
-    attr_reader
+    attr_reader :employee_id,
+                :project_id,
+                :date,
+                :minutes
     def initialize(employee_id, project_id, date, minutes)
-        @employee_id =
-        @project id =
+        @employee_id = employee_id
+        @project_id = project_id
+        @date = format_date(date)
+        @minutes = minutes.to_i
+    end
 
+    def format_date(date)
+        da = date.split('-')
+        formatted_date = Date.new(da[0].to_i, da[1].to_i, da[2].to_i)
     end
 end
--- test/time _sheet_test.rb
+++ test/time _sheet_test.rb
@@ -4,14 +4,26 @@ require './lib/employee'
 require './lib/project'
 
 class TimeSheetTest < Minitest::Test
-
+    attr_reader :time_sheet
   def setup
-    employee_id = Employee.new()
+    employee_id = '5'
     name = 'Sally Jackson'
     role = 'Engineer'
     start_date = '2015-01-01'
     end_date = '2018-01-01'
-    @employee = Employee.new(employee_id, name, role, start_date, end_date)
+    employee = Employee.new(employee_id, name, role, start_date, end_date)
+
+    project_id = '123'
+    name = 'Widget Maker'
+    start_date = '2015-01-01'
+    end_date = '2018-01-01'
+    project = Project.new('123', 'Widget Maker', '2015-01-01', '2018-01-01')
+
+    employee_id = employee.id
+    project_id = project.id
+    date = '2015-01-01'
+    minutes = '120'
+    @time_sheet = TimeSheet.new(employee_id, project_id, date, minutes)
   end
 
   def test_instantiation
@@ -19,11 +31,9 @@ class TimeSheetTest < Minitest::Test
   end
 
   def test_attributes
-    assert_equal Integer, @employee.id.class
-    assert_equal "Sally Jackson", @employee.name
-    assert_equal "Engineer", @employee.role
-    assert_instance_of Date, @employee.start_date
-    assert_instance_of Date, @employee.end_date
-    
+    assert_equal Integer, @time_sheet.employee_id.class
+    assert_equal Integer, @time_sheet.project_id.class
+    assert_equal Date, @time_sheet.date.class
+    assert_equal Integer, @time_sheet.minutes.class
   end
 end
