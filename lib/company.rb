3a35b7379fa75ffe4c8c4537a46fb5f1445b776f
John Livingston <jclivingston316@gmail.com>
2018-04-17 12:54:57 -0600

    cleaned up company test and company a little

--- lib/company.rb
+++ lib/company.rb
@@ -56,5 +56,4 @@ class Company
         end
         return status
     end
-
 end
--- test/company_test.rb
+++ test/company_test.rb
@@ -7,27 +7,7 @@ require './lib/company'
 class CompanyTest < Minitest::Test
     attr_reader :company
   def setup
-    # employee_id = '5'
-    # name = 'Sally Jackson'
-    # role = 'Engineer'
-    # start_date = '2015-01-01'
-    # end_date = '2018-01-01'
-    # employee = Employee.new(employee_id, name, role, start_date, end_date)
-
-    # project_id = '123'
-    # name = 'Widget Maker'
-    # start_date = '2015-01-01'
-    # end_date = '2018-01-01'
-    # project = Project.new('123', 'Widget Maker', '2015-01-01', '2018-01-01')
-
-    # employee_id = employee.id
-    # project_id = project.id
-    # date = '2015-01-01'
-    # minutes = '120'
-    # time_sheet = TimeSheet.new(employee_id, project_id, date, minutes)
-
     @company = Company.new
-
   end
 
   def test_instantiation
